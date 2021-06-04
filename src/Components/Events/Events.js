ddbe06cd559fd3176b3313438b592f78a6b4f1dc
NemisIGuess <ismaelpjgg@gmail.com>
2021-06-04 20:55:22 +0200

    started works on events page

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -1,6 +1,9 @@
 import './Events.css';
+import { useState } from 'react';
 
 function Events() {
+  const [events, setEvents] = useState();
+
   return (
     <div>
       <h1>Hello from Events</h1>
