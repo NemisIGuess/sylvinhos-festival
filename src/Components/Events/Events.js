e3bdd6d3c236e5a89eb4340fed260a3cf88004fd
NemisIGuess <ismaelpjgg@gmail.com>
2021-06-07 14:24:32 +0200

    small changes

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -2,13 +2,16 @@ import './Events.css';
 import { useState } from 'react';
 
 function Events() {
-  const [events, setEvents] = useState();
+  const [events, setEvents] = useState([]);
 
   return (
     <div>
       {events.map((event) => {
         return (
-          <div>
+          <div className="event-container">
+            <h3>Event name</h3>
+            <p>Event date</p>
+            <p>Event description</p>
             {/* event: h1, image, description. If you click it opens a modal with the rest of the details */}
           </div>
         );
