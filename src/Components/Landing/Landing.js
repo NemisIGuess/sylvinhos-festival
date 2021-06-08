9c01a8bc60aa5ca5a0a9a8b47a3667b58144c4b5
NemisIGuess <ismaelpjgg@gmail.com>
2021-06-08 10:07:24 +0200

    landing design

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,6 +4,7 @@ function Landing() {
   return (
     <div>
       <h1>Hello from landing</h1>
+      {/* There should be a logo, a button to sign up for the festival and the poster with the events of the festival. Include a link to socials */}
     </div>
   );
 }
