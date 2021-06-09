94c115e99499ea8855b097793c8450568b1ccdcd
NemisIGuess <ismaelpjgg@gmail.com>
2021-06-09 10:45:52 +0200

    typo

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -4,6 +4,7 @@ function FAQ() {
   return (
     <div>
       <h1>Hello from FAQ</h1>
+      {/* questions that once you click they drop down the answer, similar to the madcool festival webpage */}
     </div>
   );
 }
