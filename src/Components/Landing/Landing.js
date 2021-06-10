33624e4dc320eacb92b120c908198af8e8c92f13
NemisIGuess <ismaelpjgg@gmail.com>
2021-06-10 09:48:46 +0200

    started working on the sign up button

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,6 +4,7 @@ function Landing() {
   return (
     <div>
       <h1>Hello from landing</h1>
+      <button className="sign-up-btn">Apuntate!</button>
       {/* There should be a logo, a button to sign up for the festival and the poster with the events of the festival. Include a link to socials */}
     </div>
   );
